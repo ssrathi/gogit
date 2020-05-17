771bd27f07ecd07564b18fbd861a04eafe550e27
Shyamsunder Rathi <shyam29@gmail.com>
2020-05-17 15:37:15 -0700

    Move package doc to main.go
    Signed-off-by: Shyamsunder Rathi <shyam29@gmail.com>

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
+/*
+Implementation of git internal commands in Go language.
+
+This project is part of a learning exercise to implement a subset of "git"
+commands. It can be used to create and maintain git objects, such as blobs,
+trees, commits, branches and tags.
+
+Code Organization
+
+* "git": internal git objects and related APIs.
+* "cmd": Command line parsing and execution.
+* "util": Miscellaneous utility APIs.
+*/
 package main
 
 import (
